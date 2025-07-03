816ce7b1fd470f6e747facac215fabc257ae3d35
Prabha M <prabhamuruganantham06@gmail.com>
2025-07-03 13:40:03 +0530

    Time: 0 ms (100%), Space: 45.5 MB (19.33%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,55 +1,41 @@
-class Pair{
-    int cur;
-    int parent;
-    Pair(int c,int p){
-        this.cur = c;
-        this.parent =p;
-    }
-}
-
 class Solution {
-    public boolean bfs(int[][] graph,int source,int parent,boolean[] v,boolean[] color){
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(source,parent));
+    public boolean DFS(int[][] graph, int n, int sc, Boolean[] v, Boolean[] c, int p) {
+        v[sc] = true;
+        if (p != -1 && !c[p]) {  // Ensure parent is valid before checking color
+            c[sc] = true;
+        }
         
-        while(q.size()>0){
-            Pair rem = q.poll();
-            int c=rem.cur;
-            int p=rem.parent;
-            
-            if(!v[c]){
-                if(p==-1)
-                    color[c] = true;
-                else color[c] = !color[p];
-            }
-            else{
-                if(color[c]==color[p]){
+        for (int nbr : graph[sc]) {
+            if (v[nbr]) {
+                if (c[nbr] == c[sc]) {
                     return false;
                 }
-            }
-            v[c] = true;
-            
-            for(int neigh : graph[c]){
-
-                if(!v[neigh]){
-                    q.add(new Pair(neigh,c));
-                }
-                else{
-                    if(color[neigh]==color[c]){
-                        return false;
-                    }
+            } else {
+                if (!DFS(graph, n, nbr, v, c, sc)) {
+                    return false;
                 }
             }
         }
         return true;
-
     }
-    public boolean isBipartite(int[][] graph) {
 
+    public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        boolean[] v = new boolean[n];
-        boolean[] c = new boolean[n];
-        return bfs(graph,0,-1,v,c);
-        
+        Boolean[] c = new Boolean[n];
+        Boolean[] v = new Boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            c[i] = false;
+            v[i] = false;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!v[i]) {
+                if (!DFS(graph, n, i, v, c, -1)) { // Use -1 as the initial parent
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
