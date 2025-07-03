abefe4666b1dac4abd1ef20eb26f0e08b823ba5c
Prabha M <prabhamuruganantham06@gmail.com>
2025-07-03 13:37:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -0,0 +1,55 @@
+class Pair{
+    int cur;
+    int parent;
+    Pair(int c,int p){
+        this.cur = c;
+        this.parent =p;
+    }
+}
+
+class Solution {
+    public boolean bfs(int[][] graph,int source,int parent,boolean[] v,boolean[] color){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(source,parent));
+        
+        while(q.size()>0){
+            Pair rem = q.poll();
+            int c=rem.cur;
+            int p=rem.parent;
+            
+            if(!v[c]){
+                if(p==-1)
+                    color[c] = true;
+                else color[c] = !color[p];
+            }
+            else{
+                if(color[c]==color[p]){
+                    return false;
+                }
+            }
+            v[c] = true;
+            
+            for(int neigh : graph[c]){
+
+                if(!v[neigh]){
+                    q.add(new Pair(neigh,c));
+                }
+                else{
+                    if(color[neigh]==color[c]){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+
+    }
+    public boolean isBipartite(int[][] graph) {
+
+        int n = graph.length;
+        boolean[] v = new boolean[n];
+        boolean[] c = new boolean[n];
+        return bfs(graph,0,-1,v,c);
+        
+    }
+}
