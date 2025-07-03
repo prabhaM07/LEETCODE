9dbb96a60a10ae27d98b17033c14b40c032ff946
Prabha M <prabhamuruganantham06@gmail.com>
2025-07-03 13:41:26 +0530

    Time: 528 ms (5.63%), Space: 45.9 MB (6.92%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,41 +1,62 @@
-class Solution {
-    public boolean DFS(int[][] graph, int n, int sc, Boolean[] v, Boolean[] c, int p) {
-        v[sc] = true;
-        if (p != -1 && !c[p]) {  // Ensure parent is valid before checking color
-            c[sc] = true;
-        }
-        
-        for (int nbr : graph[sc]) {
-            if (v[nbr]) {
-                if (c[nbr] == c[sc]) {
-                    return false;
-                }
-            } else {
-                if (!DFS(graph, n, nbr, v, c, sc)) {
-                    return false;
-                }
-            }
-        }
-        return true;
+class Pair{
+    int cur;
+    int parent;
+    Pair(int c,int p){
+        this.cur = c;
+        this.parent =p;
     }
+}
 
-    public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
-        Boolean[] c = new Boolean[n];
-        Boolean[] v = new Boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            c[i] = false;
-            v[i] = false;
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (!v[i]) {
-                if (!DFS(graph, n, i, v, c, -1)) { // Use -1 as the initial parent
+class Solution {
+    public boolean bfs(int[][] graph,int source,int parent,boolean[] v,boolean[] color){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(source,parent));
+        
+        while(q.size()>0){
+            Pair rem = q.poll();
+            int c=rem.cur;
+            int p=rem.parent;
+            
+            if(!v[c]){
+                if(p==-1)
+                    color[c] = true;
+                else color[c] = !color[p];
+            }
+            else{
+                if(color[c]==color[p]){
                     return false;
+                }
+            }
+            v[c] = true;
+
+            for(int neigh : graph[c]){
+
+                if(!v[neigh]){
+                    q.add(new Pair(neigh,c));
+                }
+                else{
+                    if(color[neigh]==color[c]){
+                        return false;
+                    }
                 }
             }
         }
         return true;
+
+    }
+    public boolean isBipartite(int[][] graph) {
+
+        int n = graph.length;
+        
+        boolean[] c = new boolean[n];
+       
+        for(int i=0;i<n;i++){
+            boolean[] v = new boolean[n];
+            if(!bfs(graph,i,-1,v,c)){
+                return false;
+            }
+        }
+        return true;
+        
     }
 }
