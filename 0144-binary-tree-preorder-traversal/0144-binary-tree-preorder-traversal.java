eef2b2dd644263c9bb8be3d4d21c854089895469
Prabha M <prabhamuruganantham06@gmail.com>
2025-07-05 21:40:36 +0530

    Time: 1 ms (1.67%), Space: 42.2 MB (8.11%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -13,20 +13,47 @@
  *     }
  * }
  */
+
+class Pair{
+    TreeNode node;
+    int num;
+    Pair(TreeNode node,int num){
+        this.node = node;
+        this.num = num;
+    }
+}
+
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
+        Stack<Pair> st = new Stack<>();
+       
+        ArrayList<Integer> preOrder = new ArrayList<>();
+        if(root == null)
+         return preOrder;
+        ArrayList<Integer> inOrder = new ArrayList<>();
+        ArrayList<Integer> postOrder = new ArrayList<>();
+        st.push(new Pair(root,1));
         while(st.size()>0){
-            TreeNode t = st.pop();
-            ans.add(t.val);
-            if(t.right != null)
-            st.push(t.right);
-            if(t.left != null)
-            st.push(t.left);
+            Pair temp = st.pop();
+            
+            if(temp.num==1){
+                preOrder.add(temp.node.val);
+                temp.num++;
+                st.push(temp);
+                if(temp.node.left != null)
+                    st.push(new Pair(temp.node.left,1));
+            }
+            else if(temp.num ==2){
+                inOrder.add(temp.node.val);
+                temp.num++;
+                st.push(temp);
+                if(temp.node.right != null)
+                    st.push(new Pair(temp.node.right,1));
+            }
+            else{
+                postOrder.add(temp.node.val);
+            }
         }
-        return ans;
+        return preOrder;
     }
 }
