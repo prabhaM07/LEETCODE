2dea44f70bc73ac949fff5e544110686cccd33a1
Prabha M <prabhamuruganantham06@gmail.com>
2025-07-08 18:25:26 +0530

    Time: 116 ms (28.8%), Space: 47.3 MB (36.52%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,67 +1,48 @@
 class Solution {
-    class Pair{
-        String s;
-        int in;
-        Pair(String s,int in){
-            this.s = s;
-            this.in = in;
+    
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if(!wordList.contains(endWord)) return 0;
+        int n = wordList.size();
+        HashMap<String,Integer> hm = new HashMap<>();
+        hm.put(beginWord,0);
+        for(int i=0;i<n;i++){
+            hm.put(wordList.get(i),i+1);
         }
         
-    }
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-
-        int n = wordList.size();
-
-        if(!wordList.contains(endWord)) return 0;
-
-        HashMap<String,Integer> hm = new HashMap<>();
-        for(int i=0;i<n;i++){
-            hm.put(wordList.get(i),i);
-        }
-
-        boolean[] v = new boolean[n];
-
         Queue<String> q = new LinkedList<>();
-
         q.add(beginWord);
-   
+        boolean[] v= new boolean[n+1];
         int level = 0;
+
         while(q.size()>0){
-            int cn = q.size();
             level++;
+            int kk = q.size();
+            
+            for(int k=0;k<kk;k++){
 
-            for(int k=0;k<cn;k++){
-                //remove
                 String s = q.poll();
-                int in = -1;
 
-                if(hm.containsKey(s)){
-                 in = hm.get(s);
-                 if(v[in]) continue;
-                 v[in] = true;
-                 }
+                if(v[hm.get(s)]) continue;
+                v[hm.get(s)] = true;
 
+                if(s.equals(endWord)){
+                    return level;
+                } 
 
-                if (s.equals(endWord)) return level;
-
-
-                //add
                 for(int i=0;i<s.length();i++){
-                    for(int j =0;j<26;j++){
-                        char ch = (char)(97+j);
+                    for(int j=0;j<26;j++){
+                        char ch = (char)('a'+j);
                         StringBuilder sb = new StringBuilder(s);
                         sb.setCharAt(i,ch);
                         String ss = sb.toString();
-                        if(hm.containsKey(ss) && v[hm.get(ss)] != true){
+                        if(hm.containsKey(ss) && !v[hm.get(ss)]){
                             q.add(ss);
                         }
                     }
                 }
-                
             }
-            
-            
         }
+       
         return 0;
     }
 }
