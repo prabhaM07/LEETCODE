b9e1242369147c57b52d57dbcea2a25e87ff9fc5
Prabha M <prabhamuruganantham06@gmail.com>
2025-07-09 12:21:00 +0530

    Time: 1 ms (12.44%), Space: 42.6 MB (6.47%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -13,6 +13,15 @@
  *     }
  * }
  */
+
+class Pair{
+    TreeNode node;
+    char ch;
+    Pair(TreeNode node,char ch){
+        this.node = node;
+        this.ch = ch;
+    }
+}
 class Solution {
     boolean flag = true;
 
@@ -20,17 +29,51 @@ class Solution {
         if(root1 == null && root2 == null){
             return;
         }
+
         if(root1 == null || root2 == null || root1.val != root2.val){
             flag = false;
             return ;
         }
-        dfs(root1.left,root2.right);
-        dfs(root1.right,root2.left);
+
+        Stack<Pair> st1 = new Stack<>();
+        Stack<Pair> st2 = new Stack<>();
+        
+        st1.add(new Pair(root1,'l'));
+        st2.add(new Pair(root2,'r'));
+
+        while(st1.size()>0 && st2.size()>0){
+            Pair p1 = st1.pop();
+            Pair p2 = st2.pop();
+            
+            if(p1.node.val != p2.node.val || p1.ch == p2.ch){
+                flag = false;
+                return;
+            }
+
+            if(p1.node.left != null){
+                st1.add(new Pair(p1.node.left,'l'));  
+            }
+            if(p1.node.right != null){
+                 st1.add(new Pair(p1.node.right,'r'));
+            }
+            if(p2.node.right!=null){
+                st2.add(new Pair(p2.node.right,'r'));
+            }
+            if(p2.node.left!=null){
+                st2.add(new Pair(p2.node.left,'l'));
+            }
+            
+        }
+        if(st1.size()>0 || st2.size()>0){
+            flag = false;
+        }
+       
     }
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
         
         dfs(root.left,root.right);
+
         return flag;
     }
 }
