4b04af5ed16d3c4d4dccef4f1b7192a18a756708
Prabha M <prabhamuruganantham06@gmail.com>
2025-07-09 17:57:25 +0530

    Time: 509 ms (84.86%), Space: 0B (100%) - LeetHub

---
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -0,0 +1,11 @@
+with table_ as
+(
+    select employee_id ,department_id,count(*) over(partition by employee_id ) as cnt,primary_flag from Employee
+)
+select employee_id ,department_id  from table_ 
+WHERE 
+    CASE 
+        WHEN cnt = 1 THEN primary_flag = 'N'
+        ELSE primary_flag = 'Y'
+    END = TRUE
+;
