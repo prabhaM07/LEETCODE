2f4e873db77ac95cf92ef5b5ce2027008b8af96d
Prabha M <prabhamuruganantham06@gmail.com>
2025-07-11 09:38:26 +0530

    Time: 1 ms (98.9%), Space: 43.3 MB (65.18%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -11,18 +11,18 @@ class Solution {
     public int widthOfBinaryTree(TreeNode root) {
         if (root == null) return 0;
         
-        Deque<Pair> dq = new LinkedList<>();
+        Queue<Pair> q = new LinkedList<>();
         int ans = 1;
 
-        dq.add(new Pair(root,1));
-        while(dq.size()>0){
-            int n = dq.size();
+        q.add(new Pair(root,1));
+        while(q.size()>0){
+            int n = q.size();
             boolean flag = false;
             int left=0;
 
             for(int i=0;i<n;i++){
                 //remove
-                Pair rem = dq.poll();
+                Pair rem = q.poll();
                 int index = rem.index;
                 TreeNode node = rem.node;
 
@@ -36,10 +36,10 @@ class Solution {
 
                 //add
                 if(node.left != null){
-                    dq.add(new Pair(node.left,index*2));
+                    q.add(new Pair(node.left,index*2));
                 }
                 if(node.right != null){
-                    dq.add(new Pair(node.right,(index*2)+1));
+                    q.add(new Pair(node.right,(index*2)+1));
                 }
             }
             
