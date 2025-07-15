9acef4386b834814d7c5bb70d54ddc89b731c936
Prabha M <prabhamuruganantham06@gmail.com>
2025-07-15 11:15:06 +0530

    Time: 0 ms (100%), Space: 40.4 MB (84.75%) - LeetHub

--- 1303-minimum-moves-to-reach-target-score/1303-minimum-moves-to-reach-target-score.java
+++ 1303-minimum-moves-to-reach-target-score/1303-minimum-moves-to-reach-target-score.java
@@ -1,38 +1,20 @@
 class Solution {
     public int minMoves(int target, int maxDoubles) {
-        if(maxDoubles == 0 || target == 1) return target-1;
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
-
         int ans = 0;
-        if(target%2 ==0 && maxDoubles>0){
-            pq.add((int)(target/2));
-            maxDoubles--;
-        }
-        else
-            pq.add(target-1);
-        
-        while(pq.size()>0){
-            System.out.println(pq);
-            //remove
-            int k = pq.poll();
-
-            //work
-            ans+=1;
-            if(k<=1) break;
-            if(maxDoubles == 0){
-                ans+=(k-1);
-                break;
-            }
-           
-            //add
-            if(k % 2 ==0 && maxDoubles>0){
-                pq.add((int)(k/2));
+        while (target > 1) {
+            if (target % 2 == 0 && maxDoubles > 0) {
+                target /= 2;
                 maxDoubles--;
+                ans++;
+            } else if (maxDoubles == 0) {
+                ans += target - 1;
+                break;
+            } else {
+                target--;
+                ans++;
             }
-            else
-                pq.add(k-1);
         }
         return ans;
     }
 }
+
