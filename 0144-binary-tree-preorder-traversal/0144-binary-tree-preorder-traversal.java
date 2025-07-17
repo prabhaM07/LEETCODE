1b87902dffb87662ae0b2beab82d0f3994ddbe34
Prabha M <prabhamuruganantham06@gmail.com>
2025-07-17 15:55:08 +0530

    Time: 0 ms (100%), Space: 41.6 MB (82.56%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,46 +14,58 @@
  * }
  */
 
-class Pair{
-    TreeNode node;
-    int num;
-    Pair(TreeNode node,int num){
-        this.node = node;
-        this.num = num;
-    }
-}
-
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        Stack<Pair> st = new Stack<>();
-       
-        ArrayList<Integer> preOrder = new ArrayList<>();
-        if(root == null)
-         return preOrder;
-        ArrayList<Integer> inOrder = new ArrayList<>();
-        ArrayList<Integer> postOrder = new ArrayList<>();
-        st.push(new Pair(root,1));
-        while(st.size()>0){
-            Pair temp = st.pop();
-            
-            if(temp.num==1){
-                preOrder.add(temp.node.val);
-                temp.num++;
-                st.push(temp);
-                if(temp.node.left != null)
-                    st.push(new Pair(temp.node.left,1));
+
+    public TreeNode last(TreeNode proot,TreeNode root){
+        
+        while(true){
+            if(root.right == null && root.left == null) break;
+
+            if(root.right !=null ){
+                if(root.right == proot){
+                    break;
+                }
+                else
+                    root = root.right;
             }
-            else if(temp.num ==2){
-                inOrder.add(temp.node.val);
-                temp.num++;
-                st.push(temp);
-                if(temp.node.right != null)
-                    st.push(new Pair(temp.node.right,1));
-            }
-            else{
-                postOrder.add(temp.node.val);
+            else if(root.left != null){
+                if(root.left == proot){
+                    break;
+                }
+                else
+                    root = root.left;
             }
         }
-        return preOrder;
+        return root;
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+       List<Integer> ans = new ArrayList<>();
+        TreeNode temp = root;
+
+        while(root!=null){
+            ans.add(root.val);
+
+            if(root.left!=null && root.right!=null){
+              
+                TreeNode t = last(root,root.left);
+
+                if(t.right != root.right)
+                {
+                    t.right = root.right;
+                    root = root.left;
+                    continue;
+                }
+                else{
+                    t.right = null;
+                }
+            }
+            else{
+                if(root.right!=null)root = root.right;
+                else root = root.left;
+            }
+            
+        }
+        return ans;
     }
 }
